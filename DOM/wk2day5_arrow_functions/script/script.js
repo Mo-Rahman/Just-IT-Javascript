d3d4049c9796e6f44d9aaba7afc68512abe4a6df
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2023-02-09 22:19:20 +0000

    Some copy and paste code the teacher did - not entirely correct either

---
+++ DOM/wk2day5_arrow_functions/script/script.js
@@ -0,0 +1,15 @@
+// document.getElementById("example7").addEventListener("click", function () {
+//   document.getElementById("example7").innerHTML = "Something Else";
+//   alert("i've been clicked");
+// });
+
+document.getElementById("example7").addEventListener("click", (e) => {
+  e.currentTarget.innerHTML = "Hello world";
+  // console.log(e.currentTarget.alert("HELLO"));
+  // e.currentTarget.alert("I've been clicked");
+});
+
+// document.getElementById("btn").addEventListener("click", (e) => {
+//   // e.currentTarget.innerHTML = "Something else";
+//   e.currentTarget.alert("I've been clicked");
+// });
---
+++ DOM/wk2day5_math_object/script/scripts.js
@@ -0,0 +1,21 @@
+//Preconditions: Takes two parameters, thh lowest number min and a highest number max
+//Postconditions: Returns a random number from min to max.
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+10 - 1 + 1;
+
+array.forEach((element) => {
+  element.addEventlister("click", function () {});
+});
+
+array.forEach((element) => {
+  element.addEventlister("click", function () {
+    // Do something here!
+  });
+});
+
+// - 2 input boxes one for min number one for max
+// - Button with click event to trigger function
+// - Displays a random number on screen between min and max
