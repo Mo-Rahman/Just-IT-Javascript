7c47fcfd7d7d47ab4f4f107ebb3382d0b26dad1f
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2023-02-10 12:28:13 +0000

    Experimented with what I thought was an event click using an arrow function and had the same because as a regular function in a click event

--- DOM/day-04/app.js
+++ DOM/day-04/app.js
@@ -5,13 +5,22 @@
 
 // Event binding solution.
 
-document.getElementById("example7").addEventListener("click", (e) => {
-  e.currentTarget.innerHTML = "Hello world";
-  // console.log(e.currentTarget.alert("HELLO"));
-  // e.currentTarget.alert("I've been clicked");
-});
+// I didn't have to bind the event to the click for it to work
 
-// document.getElementById("btn").addEventListener("click", (e) => {
-//   // e.currentTarget.innerHTML = "Something else";
-//   e.currentTarget.alert("I've been clicked");
-// });
+document.getElementById("example7").addEventListener("click", (e) => {
+  // e.currentTarget.innerHTML = "Hello world";
+  // e.target.innerHTML = "something else"; // Now to change the innerHTML using target.
+  // alert("I've been clicked");
+  // document.getElementById("example7").style.color = "red";
+  document.getElementById("example7").style.color = "red";
+  console.log(e); // see the value of "e" in console
+  console.log(e.target);
+  console.log(`e.target: ${e.target}`); // the I get <p id="example7">something else</p>
+  console.log(e.target.innerHTML); // to target the p tag use innerHTML - see console for result
+  e.target.innerHTML = "I've been clicked and changed!"; // Now to change the innerHTML using target.
+
+  // What happens when you have two returnable lines of code?
+
+  // e.target.style.color = "red"; // Example to change style
+  console.log(e.target.innerHTML); // See the new value for innerHTML after the pointer click!
+});
