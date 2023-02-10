7d5c119429a4b2cc90712b85e86b65bcd93a08fe
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2023-02-10 22:07:46 +0000

    empty day 5 as we didn't actually cover anything

--- DOM/day-04/app.js
+++ DOM/day-04/app.js
@@ -7,15 +7,15 @@
 
 // I didn't have to bind the event to the click for it to work
 
-document.getElementById("example7").addEventListener("click", (e) => {
-  // e.currentTarget.innerHTML = "Hello world";
+document.getElementById("example7").addEventListener("click", function (e) {
+  e.currentTarget.innerHTML = "Hello world";
   // e.target.innerHTML = "something else"; // Now to change the innerHTML using target.
   // alert("I've been clicked");
   // document.getElementById("example7").style.color = "red";
   document.getElementById("example7").style.color = "red";
   console.log(e); // see the value of "e" in console
   console.log(e.target);
-  console.log(`e.target: ${e.target}`); // the I get <p id="example7">something else</p>
+  // console.log(`e.target: ${e.target}`); // the I get <p id="example7">something else</p>
   console.log(e.target.innerHTML); // to target the p tag use innerHTML - see console for result
   e.target.innerHTML = "I've been clicked and changed!"; // Now to change the innerHTML using target.
 
@@ -33,6 +33,8 @@ document.querySelectorAll("#btn-list button").forEach((element) => {
     console.log(e);
   });
 });
+
+// document.querySelector("")
 
 // document.querySelectorAll("#btn-list button").forEach((element) => {
 //   element.addEventListener("click", function (e) {
